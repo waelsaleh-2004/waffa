78b4db10c2707c4bae01fe32716e3ef23d17a650
waelsaleh-2004 <salhwael92@gmail.com>
2026-06-29 10:21:33 +0300

    تجهيز ورفع ملف main.js والمجلد الصوتي

--- main.js
+++ main.js
@@ -52,6 +52,7 @@ const correctAnswerHint = document.getElementById("correctAnswerHint");
 const successSound = new Audio("./audio/meldix-success-340660.mp3");
 const wrongSound = new Audio("./audio/u_8g40a9z0la-fail-234710.mp3");
 
+
 // 🎲 دالة ترتيب الأسئلة عشوائياً
 function shuffleQuestions() {
     quizData.sort(() => Math.random() - 0.5);
